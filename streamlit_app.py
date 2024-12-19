e9bc76221e6edfe3340fa9132161c25e8b110d6a
Dimas Betioli Ribeiro <69821362+dimasbetioli@users.noreply.github.com>
2024-12-19 15:18:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ if tipo_entrada == "Inserir manualmente":
 elif tipo_entrada == "Carregar arquivo Excel":
     st.write("Você escolheu carregar um arquivo Excel.")
 else:
-    st.write("Por favor, selecione uma opção para continuar.")
+    st.write("Por favor, selecione acima se deseja inserir os dados manualmente ou carregar um arquivo Excel &#8593;.")
 
 if tipo_entrada == "Inserir manualmente":
     # Opções de configuração
